24b44f6f2c72e36f40b36215d995d7918be6908f
jesseduijndam <50326150+jesseduijndam@users.noreply.github.com>
2019-12-17 14:53:03 +0100

    just the end tag

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,12 @@
 <?php
 
-// General settings
-$host       = "localhost";
-$database   = "hair4u";
-$user       = "root";
-$password   = "";
+    // General settings
+    $host       = "localhost";
+    $database   = "hair4u";
+    $user       = "root";
+    $password   = "";
 
-$db = mysqli_connect($host, $user, $password, $database)
-    or die("Error: " . mysqli_connect_error());;
+    $db = mysqli_connect($host, $user, $password, $database)
+        or die("Error: " . mysqli_connect_error());;
+    
+?>
